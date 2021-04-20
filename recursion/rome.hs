1219e3e058e004a8b5d64cc1e245ba13bf65954d
Evan Tian <17511710+evantianx@users.noreply.github.com>
2021-04-20 10:24:56 +0800

    recursion: fix rome

--- recursion/rome.hs
+++ recursion/rome.hs
@@ -7,7 +7,7 @@ romeAmount :: [Int]
 romeAmount = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
 pair :: [(Int, String)]
-pair = zip romeAnount romeNotation
+pair = zip romeAmount romeNotation
 
 subtrahend :: Int -> (Int, String)
 subtrahend n = head (dropWhile (\(a, _) -> a > n) pair)
@@ -15,4 +15,5 @@ subtrahend n = head (dropWhile (\(a, _) -> a > n) pair)
 convert :: Int -> String
 convert 0 = ""
 convert n = let (rome, m) = subtrahend n
-            in m ++ convert (n-rome)
+                rest = n - rome
+            in m ++ convert (rest)
