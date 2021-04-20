8c4c7a2cf13dc7098b9fda3ffad039557d2b5de2
Evan Tian <17511710+evantianx@users.noreply.github.com>
2021-04-20 10:58:18 +0800

    recursion: update insert sort

--- recursion/insert.hs
+++ recursion/insert.hs
@@ -2,3 +2,7 @@ insert :: (Ord a) => a -> [a] -> [a]
 insert x [] = [x]
 insert x (y:ys) | x <= y = x:y:ys
                 | otherwise = y:insert x ys
+
+insertSort :: Ord a => [a] -> [a] -> [a]
+insertSort xs [] = xs
+insertSort xs (y:ys) = insertSort (insert y xs) ys
