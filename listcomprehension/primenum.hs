5b8a00c241c7a71f1f543eec1b457a42adc1cd27
Evan Tian <17511710+evantianx@users.noreply.github.com>
2021-04-21 11:21:43 +0800

    Create primenum.hs

---
+++ listcomprehension/primenum.hs
@@ -0,0 +1,15 @@
+-- too slow..
+factors :: Integral a => a -> [a]
+factors n = [x | x <- [1..n], mod n x == 0]
+
+isPrime :: Integral a => a -> Bool
+isPrime n = factors n == [1, n]
+
+primes :: Integral a => a -> [a]
+primes n = [x | x <- [1..n], isPrime x]
+
+-- 2nd version
+isPrime' :: Integral a => a -> Bool
+isPrime' 2 = True
+isPrime' p =
+  p > 1 && (all (\n -> p `mod` n /= 0) $ takeWhile (\n -> n * n <= p) [3, 5..])
