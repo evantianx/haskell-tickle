98a3940b4150a76d3b3a8341a2678c877da1e02f
Evan Tian <17511710+evantianx@users.noreply.github.com>
2021-04-21 11:50:15 +0800

    Update primenum.hs

--- listcomprehension/primenum.hs
+++ listcomprehension/primenum.hs
@@ -18,5 +18,5 @@ isPrime' p =
 sieve :: Integral a => [a] -> [a]
 sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
-primes :: Integral a => ([a] -> [a]) -> [a]
+primes :: Integral a => ([a] -> [a]) -> [a] -> [a]
 primes' = sieve [2..]
